5df4004d2d4587fc5aee57d78078ad6c5f6f09d0
yansiqi01 <3237840945@qq.com>
2019-05-29 18:52:52 +0800

    上传页面

--- src/main/java/com/zking/controller/UserController.java
+++ src/main/java/com/zking/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.zking.controller;
 
-import com.zking.model.User;
 import com.zking.redis.RedisTemplateUtil;
 import com.zking.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,9 @@ import javax.annotation.Resource;
 public class UserController {
 
     private static final String KEY = "a";
+
+    //声明一个日志
+    private static Logger logger = LogManager.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -32,16 +36,24 @@ public class UserController {
     @ResponseBody
     @RequestMapping("/test")
     public String test(){
+
         System.out.println("测试成功。。。");
         return userService.getUsers()+"";
     }
+
+
+
 
 
     //测试缓存
     @RequestMapping("/redis")
     public String b(){
 
-        System.out.println("数据打印："+redisTemplate.opsForValue().get("user1"));
+//        System.out.println("数据打印："+redisTemplate.opsForValue().get("user1"));
+        System.out.println(redisTemplateUtil.getString("user1"));
+  /*      logger.info("===========================================执行了方法");
+        logger.error("==========================================执行了异常日志打印");
+        logger.debug("==========================================执行了debug日志打印");*/
 //        System.out.println("缓存数据："+redisTemplateUtil.getString(KEY));
 
 //        System.out.println("缓存数据:"+redisTemplate.opsForValue().get(KEY).toString());
