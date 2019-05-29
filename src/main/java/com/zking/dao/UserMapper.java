256877241b4bf3511754894343fab32633d413ec
yansiqi01 <3237840945@qq.com>
2019-05-29 21:09:57 +0800

    新增redis作为mybatis二级缓存

--- src/main/java/com/zking/dao/UserMapper.java
+++ src/main/java/com/zking/dao/UserMapper.java
@@ -1,7 +1,6 @@
 package com.zking.dao;
 
 import com.zking.model.User;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +10,9 @@ public interface UserMapper {
 
 
     //查询所有用户
-    @Select("Select * from t_user")
     List<User> getUsers();
+
+
 
 
 }
