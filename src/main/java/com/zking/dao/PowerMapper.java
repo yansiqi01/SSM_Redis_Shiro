b60bebe3725c442709d2450a6b788879fc9d94af
yansiqi01 <3237840945@qq.com>
2019-05-31 00:10:05 +0800

    添加后台权限功能

---
+++ src/main/java/com/zking/dao/PowerMapper.java
@@ -0,0 +1,29 @@
+package com.zking.dao;
+
+import com.zking.model.Power;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 权限菜单
+ */
+@Service("powerMapper")
+public interface PowerMapper {
+
+    /**
+     * 根据员工id查询对应的权限
+     * @param uid
+     * @return
+     */
+    List<Power> getPowerByUserId(@Param("uid") Integer uid);
+
+    /**
+     * 根据父id查询tree树
+     * @param fid
+     * @return
+     */
+    List<Power> getPowerByUserTreeAndFid(@Param("fid")Integer fid);
+
+}
