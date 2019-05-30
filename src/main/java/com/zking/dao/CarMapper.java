ba8c0438ab3c7becebded0cd19f3300ee3d60d36
ismyprincess <921607915@qq.com>
2019-05-30 21:50:01 +0800

    carmapper

---
+++ src/main/java/com/zking/dao/CarMapper.java
@@ -0,0 +1,61 @@
+package com.zking.dao;
+
+import com.zking.model.Car;
+import com.zking.model.CarType;
+import com.zking.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @date 2019-05-30 15:14
+ * @author likai
+ */
+@Service("CarMapper")
+public interface CarMapper {
+
+
+    /**
+     * 查询所有的车辆信息
+     * @return
+     */
+    List<Car> getCars();
+
+    /**
+     * 根据车牌号查询车辆信息
+     * @param workNo
+     * @return
+     */
+    Car getCarByWorkNo(String workNo);
+
+    /**
+     * 根据车辆状态查询车辆信息
+     * @param state
+     * @return
+     */
+    List<Car> getCarsByState(Integer state);
+
+    /**
+     * 得到所有的车辆类型，供添加车辆时选择车辆类型
+     * @return
+     */
+    List<CarType> getCarTypes();
+
+    /**
+     * 根据车辆ID修改车辆状态
+     * @param carId
+     * @return
+     */
+    int updateCarState(Integer carId);
+
+    /**
+     * 增加车辆信息
+     * @param car
+     * @return
+     */
+    int addCar(Car car);
+
+
+
+
+}
