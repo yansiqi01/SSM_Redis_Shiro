c158692be05211232c8678f26c8a4339c5b526cc
ismyprincess <921607915@qq.com>
2019-05-30 18:07:43 +0800

    驼峰

--- src/main/java/com/zking/model/Car.java
+++ src/main/java/com/zking/model/Car.java
@@ -8,15 +8,15 @@ import java.io.Serializable;
 public class Car implements Serializable {
 
     private static final long serialVersionUID = 1278240035261357193L;
-    private  Integer CarId;//车ID
-    private  String  WorkNo;//车牌号
+    private  Integer carId;//车ID
+    private  String  workNo;//车牌号
     private  CarType carType;//外键（车类型ID）
-    private  Double  CarSize;//车容量
-    private  Double CarTonnage;//车吨位
-    private  String BuyTiem;//购车时间
-    private  String CarLeavetime;//发车时间
-    private  Integer CarstateId;//车辆状态
+    private  Double  carSize;//车容量
+    private  Double carTonnage;//车吨位
+    private  String buyTiem;//购车时间
+    private  String carLeavetime;//发车时间
+    private  Integer carStateId;//车辆状态
     private  Line line;//外键(线路ID)
     private  Employee employee;//外键（员工ID）
-    private  String CarRemark;//备注
+    private  String carRemark;//备注
 }
