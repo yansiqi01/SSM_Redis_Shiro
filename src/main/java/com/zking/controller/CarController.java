3b457a412b1c3cb556ab061a92d1d8c050b9ffe5
ismyprincess <921607915@qq.com>
2019-05-31 09:46:04 +0800

    car

---
+++ src/main/java/com/zking/controller/CarController.java
@@ -0,0 +1,42 @@
+package com.zking.controller;
+
+import com.zking.model.Car;
+import com.zking.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @ClassName CarController
+ * @Author likai
+ * @Date
+ **/
+@Controller
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+
+    /**
+     * 得到所有的车辆信息 或者根据车牌号查询
+     * 根据车辆状态查询
+     * shuttleBusSet.html绑定数据
+     * @return
+     */
+    @RequestMapping("/getCars")
+    @ResponseBody
+    public List<Car> getCars(){
+        //从前台接受
+        String workNo = "6";
+        List<Car> cars = carService.getCars(workNo,1);
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+        return cars;
+    }
+
+}
--- src/main/java/com/zking/service/CarService.java
+++ src/main/java/com/zking/service/CarService.java
@@ -1,21 +1,23 @@
-package com.zking.dao;
+package com.zking.service;
 
 import com.zking.model.Car;
 import com.zking.model.CarType;
 import com.zking.model.User;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("carMapper")
-public interface CarMapper {
-
+/**
+ * @ClassName CarService
+ * @Author likai
+ * @Date
+ **/
+public interface CarService {
     /**
      * 查询所有的车辆信息
      * @return
      */
-    List<Car> getCars(@Param("workNo") String workNo,@Param("carStateId")Integer carStateId);
+    List<Car> getCars(@Param("workNo")String workNo,@Param("carStateId")Integer carStateId);
 
     /**
      * 得到所有的车辆类型，供添加车辆时选择车辆类型
@@ -36,7 +38,4 @@ public interface CarMapper {
      * @return
      */
     int addCar(Car car);
-
-
-
 }
