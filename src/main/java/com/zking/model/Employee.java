1ea906904cdf6430bee45d91391bbd7f96f6c994
yansiqi01 <3237840945@qq.com>
2019-06-03 13:27:54 +0800

    更新实体类

--- src/main/java/com/zking/model/Employee.java
+++ src/main/java/com/zking/model/Employee.java
@@ -7,15 +7,15 @@ import java.io.Serializable;
 @Data
 public class Employee implements Serializable {
     private static final long serialVersionUID = -2277950635615433893L;
-    private Integer EmpId;//员工ID
-    private String EmpName;//员工姓名
-    private String EmpSex;//员工性别
-    private String EmpAddress;//员工地址
-    private String EmpTel;//员工电话
-    private String EmpWorkTime;//入职时间
-    private Double EmpSalary;//员工月薪
-    private String EmpCardId;//员工身份证
-    private Position position;//外键(职位ID)
+    private Integer empId;//员工ID
+    private String empName;//员工姓名
+    private String empSex;//员工性别
+    private String empAddress;//员工地址
+    private String empTel;//员工电话
+    private String empWorkTime;//入职时间
+    private Double empSalary;//员工月薪
+    private String empCardId;//员工身份证
+//    private Position position;//外键(职位ID)
     private Deliveryspot deliveryspot;//外键(所属配送点)
-    private Integer EmpFiag;//任职状态
+    private Integer empFiag;//任职状态
 }
